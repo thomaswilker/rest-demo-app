22f98349f9b201e5532ce712b3aba6dd398cfd13
unknown <t_wilker@LT57-62.ads.tu-darmstadt.de>
2016-06-07 14:07:10 +0200

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name restDemoAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the restDemoAppApp
+ */
+angular.module('restDemoAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
